2518abc7e3f05016ba4fbaf8cb07c440b0ad0ee8
Alvaro Maluenda <amaluend@gmail.com>
2022-10-15 12:11:58 -0300

    Ejercicio tarea para sábado 15 (contador de votos)

--- contador_votos.py
+++ contador_votos.py
@@ -1,14 +1,56 @@
 #Contador de votos online
-def limpiar_pantalla():
+from sys import flags
+
+
+def pantalla():
     import os
-    return os.system("cls")
+    return os.system("clear")
+
+def republicano():
+    global repu
+    pantalla()
+    repu = repu + 1
+    
+
+def democrata():
+    global demo
+    pantalla()
+    demo = demo + 1
+
+def total(repu, demo):
+    print(f"El total de votos republicanos a la fecha es : {repu}")
+    print(f"El total de votos demócratas a la fecha es : {demo}")
+    return
+
+def salir():
+    global flag
+    pantalla()
+    flag = True
+
+pantalla()
+flag = False
+repu = 0
+demo = 0
 
 while not flag:
-    print("*** MENU ***")
+    print("         *** MENU ***")
+    print("=============================")
     print("[1] -> Partido Republicano")
     print("[2] -> Partido Demócrata")
     print("[3] -> Total de votos")
-    print("[4] -> Salir")
+    print("[4] -> Salir del sistema")
+    print("=============================")
+    print("")
+    opt = int(input("Ingrese una opción : "))
 
+    if opt == 1:
+        republicano()
 
-if opt = 1
+    elif opt == 2:
+        democrata()
+
+    elif opt == 3:
+        total(repu, demo)
+
+    else:
+        salir()

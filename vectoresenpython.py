ca326a71c4f9d24bb038b0f341eb86b813f2874a
Alvaro <al.maluenda@duocuc.cl>
2022-11-19 13:30:32 -0300

    ejercicio en clase 3.2 con IoT en packet tracer

---
+++ vectoresenpython.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+x = np.linspace(-np.pi, np.pi, 10)
+print x
+
+print x[0]  # first element
+print x[2]  # third element
+print x[-1] # last element
+print x[-2] # second to last element
